dddfe6e48faa036a7688cb0ec5921e988495749b
Rob Westgeest <rob@qwan.it>
2011-06-22 14:48:54 +0200

    added delivering for free vendingmachine

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,3 +1,24 @@
-class VendingMachine
+require 'enum'
 
+class Choice < Enum
+  values :cola, :fanta
+end
+
+class Can < Enum
+  values :nothing, :cola, :fanta
+end
+
+class VendingMachine
+  def initialize
+    @drawers = {}
+    @drawers.default = Can.nothing
+  end
+
+  def add_choice(choice, can)
+    @drawers[choice] = can
+  end
+
+  def deliver(choice)
+    return @drawers[choice]
+  end
 end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -3,7 +3,21 @@ require File.join(File.dirname(__FILE__),'spec_helper')
 require 'vending_machine'
 
 describe VendingMachine do
-  it "delivers nothing" do
-    1.should == 0
+  describe "delivery" do
+    let(:vending_machine) { VendingMachine.new }
+
+    before(:each) do
+      vending_machine.add_choice(Choice.cola, Can.cola)
+      vending_machine.add_choice(Choice.fanta, Can.fanta)
+    end
+
+    it "delivers can of choice" do
+      vending_machine.deliver(Choice.cola).should == Can.cola
+      vending_machine.deliver(Choice.fanta).should == Can.fanta
+    end
+
+    it "delivers nothing on illegal choice" do
+      vending_machine.deliver('bogus_choice').should == Can.nothing
+    end
   end
 end
