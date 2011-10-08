eb9e277a483eeb4580a83b9d178c154059fee701
Rob Westgeest <rob.westgeest@gmail.com>
2011-10-08 10:27:29 +0200

    updated deployer

--- lib/deploy/deployer.rb
+++ lib/deploy/deployer.rb
@@ -1,11 +1,36 @@
+
 require 'fileutils'
 module Deploy
+  def self.deploy(file)
+    Deployer.new(file).deploy
+  end
   class Deployer
+    include FileUtils
     DEPLOYED_DIR = File.expand_path("~/.vendingmachine")
-    def deploy(file_path)
-      FileUtils.cp file_path, File.join(DEPLOYED_DIR, File.basename(file_path))
-      require File.join(DEPLOYED_DIR, File.basename(file_path))
+
+    attr_reader :file_path
+
+    def initialize(file_path)
+      @file_path = file_path
+      $: << DEPLOYED_DIR unless $:.include?(DEPLOYED_DIR)
     end
 
+    def deploy
+      rm_r Dir.glob(File.join(DEPLOYED_DIR, '*'))
+      cd(DEPLOYED_DIR) do
+        system "unzip #{file_path}"
+      end
+
+      load_classes
+    end
+
+    def load_classes
+      Object.send(:remove_const, "ControlCode") if defined?(ControlCode)
+      self.class.module_eval " module ::ControlCode; end "
+      Dir.glob(File.join(DEPLOYED_DIR, '*')).each do |file|
+        ControlCode.module_eval(File.read(file) )
+      end
+    end
   end
 end
+
