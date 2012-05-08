d65f43eb996a5ddc083f63b151aeecb635330aa9
Rob Westgeest <rob.westgeest@gmail.com>
2012-05-08 19:57:00 +0200

    added first cucumber spec

--- features/steps/vending_machine_steps.rb
+++ features/steps/vending_machine_steps.rb
@@ -1,1 +1,14 @@
 
+Given /^a machine with cola and sprite$/ do
+  @bin = Bin.new
+  @vendingmachine = VendingMachine.new(@bin)
+end
+
+When /^I choose cola$/ do
+  @vendingmachine.choose(Choice.cola)
+end
+
+Then /^A can of cola is delivered$/ do
+  @bin.include?(Can.cola)
+end
+
--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,2 +1,28 @@
 require 'enum'
+class Choice < Enum
+  values :cola
+end
+class Can < Enum
+  values :cola
+end
+class Bin
+  def initialize
+    @content = []
+  end
+  def catch(can)
+    @content << can
+  end
+  def include?(can)
+    return @content.include?(can)
+  end
+end
+
+class VendingMachine
+  def initialize bin
+    @bin = bin
+  end
+  def choose(choice)
+    @bin.catch(Can.cola)
+  end
+end
 
