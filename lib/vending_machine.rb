2aa39f5697dae8dbec13f733612e5e986297d613
Rob Westgeest <rob.westgeest@gmail.com>
2012-05-08 19:18:55 +0200

    added lib in path and created CucumberExercise branch

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,25 +1,2 @@
 require 'enum'
 
-class VendingMachine
-    def initialize
-        @cans = {}
-        @cans.default = Can.none
-    end
-    
-    def deliver(choice)
-        return @cans[choice]
-    end
-    
-    def configure(choice, can)
-        @cans[choice] = can
-    end
-end
-
-class Can < Enum
-    values :none, :cola, :sprite
-end
-
-class Choice < Enum
-    values :cola, :fanta, :sprite
-end
-
